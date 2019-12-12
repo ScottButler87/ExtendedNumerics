11999a33d9825f75ad788cb3d305efbc8572b59b
Scott Butler <scottbutler87@gmail.com>
2019-12-12 00:09:47 -0700

    Removed mentions of trimmed dependency GMP in makefiles.

--- wasm_make.sh
+++ wasm_make.sh
@@ -2,6 +2,6 @@
   # shellcheck source=/home/scott/emsdk/emsdk_env.sh
 source ~/emsdk/emsdk_env.sh
 cd wasm_build || exit
-GMPDIR=~/opt emconfigure /usr/local/bin/cmake -DBUILD_WASM=True -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-s ALLOW_MEMORY_GROWTH=1 -s ENVIRONMENT=node -s STRICT=0 -s STACK_OVERFLOW_CHECK=1 -O3 -DNDEBUG" --with-gmp=${HOME}/opt ..
+emconfigure /usr/local/bin/cmake -DBUILD_WASM=True -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-s ALLOW_MEMORY_GROWTH=1 -s ENVIRONMENT=node -s STRICT=0 -s STACK_OVERFLOW_CHECK=1 -O3 -DNDEBUG" ..
 emmake make -j6
 cd ..
